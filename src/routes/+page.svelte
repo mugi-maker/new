142694a1e5a1ea21aee7ab4d7b0bec0e1983399b
admin755 <editztop9@gmail.com>
2024-09-14 17:11:08 +0530

    Added country form with Supabase integration

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let data;
+  </script>
+
+
+
+
+
+  <div class="overflow-x-auto">
+    <table class="table table-zebra">
+      <!-- head -->
+      <thead>
+        {#each data.countries as country}
+        <tr>
+          <th><label>
+            <input type="checkbox" class="checkbox" />
+          </label></th>
+          <th>{country.id}</th>
+          <th>{country.name}</th>
+        </tr>
+        {/each}
+      </thead>
+      <tbody>
+        <!-- row 1 -->
+       
+  
+      </tbody>
+    </table>
+  </div>
